9c6bfcdc0c0752723f194ce68b23ad26858a9b5d
lxw <lxwAddress>
2019-05-20 16:17:35 +0800

    Ok

---
+++ src/com/ahead/demo.java
@@ -0,0 +1,1 @@
+package com.ahead;/** * @author ：lxwnb * @date ：Created in 2019/5/20 16:07 * @desc : {} */public class demo {	public static void main(String args[]){		System.out.println("Hello");		}}
